bac7bae58a0b3e657c85de26efa02eafa63aadb6
julien.coron <julien.coron@gmail.com>
2012-11-28 21:15:48 +0000

    Licencing + optimisation

--- js/ganttCalendar-src.js
+++ js/ganttCalendar-src.js
@@ -1,6 +1,31 @@
 /*
  * Gantt Calendar by Julien CORON - 2012
- *  
+ * Copyright 2012 Julien CORON
+ * 
+ * Licence :
+   Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le 
+   modifier suivant les termes de la GNU General Public License telle que 
+   publiée par la Free Software Foundation ; soit la version 3 de la licence, 
+   soit (à votre gré) toute version ultérieure.
+
+   Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE 
+   GARANTIE ; pas même la garantie implicite de COMMERCIABILISABILITÉ ni 
+   d'ADÉQUATION à UN OBJECTIF PARTICULIER. Consultez la GNU General Public 
+   License pour plus de détails.
+ * 
+ * 
+   This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 TimeLineMonth = function(container, month, year, ressources, updateMonthCallback) {
 	return this.init(container, month, year, ressources, updateMonthCallback);
@@ -33,75 +58,79 @@ $.extend(TimeLineMonth.prototype, {
 	},
 
 	drawElements: function() {
-		var containerObj = $("#"+this.container);
+		var containerObj, largeCalendar, monthLine, calendarHeaders, eventsContainer, headerRessources, 
+			spanLabelCenter, listRessources, indexGroup, group, indexRessource,
+			ressource, bisextile, horizontalCalendarContent, lineOfDays, htmlDays, 
+			indexDay, day2digits;
+		containerObj = $("#"+this.container);
 		containerObj.timeLineMonth = this;
 		
-		var largeCalendar = $( document.createElement('div') ).addClass("largeCalendar");
+		largeCalendar = $( document.createElement('div') ).addClass("largeCalendar");
 		containerObj.html(largeCalendar);
 		
-		var monthLine = $( document.createElement('div') ).addClass("month");
+		monthLine = $( document.createElement('div') ).addClass("month");
 		largeCalendar.html(monthLine);
 		monthLine.html("<div class=\"prevMonth\" title=\"[Page down] Go to Previous month\"></div>\
 		<div class=\"nameMonth\">"+this.months[this.lang][this.month-1]+" "+this.year+"</div>\
 		<div class=\"nextMonth\" id=\"nextMonth\" title=\"[Page up] Go to next month\"></div>");
 		
-		var calendarHeaders = $( document.createElement('div') ).addClass("leftColumn horizontalCalendarHeaders");
+		calendarHeaders = $( document.createElement('div') ).addClass("leftColumn horizontalCalendarHeaders");
 		largeCalendar.append(calendarHeaders);
 		
-		var eventsContainer = $( document.createElement('div') ).addClass("eventsContainer");
+		eventsContainer = $( document.createElement('div') ).addClass("eventsContainer");
 		largeCalendar.append(eventsContainer);
 		
-		var headerRessources = $( document.createElement('div') ).addClass("headerRessources");
+		headerRessources = $( document.createElement('div') ).addClass("headerRessources");
 		calendarHeaders.html(headerRessources);
 
 		// update the size of eventsContainer to match with borders
 		eventsContainer.width(largeCalendar.width() - headerRessources.width() - 1);
 
-		var spanLabelCenter = $( document.createElement('span') ).addClass("labelCenter").html(this.ressourcesColumnHeader);
+		spanLabelCenter = $( document.createElement('span') ).addClass("labelCenter").html(this.ressourcesColumnHeader);
 		headerRessources.html(spanLabelCenter);
 		
-		var listRessources = $( document.createElement('div') ).addClass("listRessources");
+		listRessources = $( document.createElement('div') ).addClass("listRessources");
 		calendarHeaders.append(listRessources);
 		
 		
 		// All groups and ressources, prepare left side
-		for(var indexGroup=0;indexGroup<this.ressources.groups.length;indexGroup++){
-			var group = this.ressources.groups[indexGroup];
+		for(indexGroup=0;indexGroup<this.ressources.groups.length;indexGroup++){
+			group = this.ressources.groups[indexGroup];
 			listRessources.append("<div id=\"group_"+group.id+"\" class=\"group\">\
 					<span class=\"labelLeft\">"+group.name+"</span>\
 				</div>");
 			
-			for(var indexRessource=0;indexRessource<group.ressources.length;indexRessource++){
-				var ressource = group.ressources[indexRessource];
+			for(indexRessource=0;indexRessource<group.ressources.length;indexRessource++){
+				ressource = group.ressources[indexRessource];
 				listRessources.append("<div id=\"ressource_"+ressource.id+"\" data-group=\"group_"+group.id+"\" class=\"ressource lineRessource\">\
 					<span class=\"labelRight\">"+ressource.name+"</span>\
 					</div>");
 			}
 		}
 		
-		var bisextile = ( (this.year%4==0 && this.year%100!=0) || this.year%400==0 )?(1):(0);
-		var horizontalCalendarContent = $( document.createElement('div') )
+		bisextile = ( (this.year%4==0 && this.year%100!=0) || this.year%400==0 )?(1):(0);
+		horizontalCalendarContent = $( document.createElement('div') )
 			.addClass("horizontalCalendarContent for"+this.days[this.month][bisextile]+"days")
 			.attr("tabindex", "0");
 		eventsContainer.append(horizontalCalendarContent);
 		
-		var lineOfDays = $( document.createElement('div') ).addClass("lineOfDays");
+		lineOfDays = $( document.createElement('div') ).addClass("lineOfDays");
 		horizontalCalendarContent.html(lineOfDays);
 		
-		var htmlDays = "";
-		for(var indexDay=1;indexDay<=this.days[this.month][bisextile];indexDay++){
-			var day2digits = indexDay;
+		htmlDays = "";
+		for(indexDay=1;indexDay<=this.days[this.month][bisextile];indexDay++){
+			day2digits = (indexDay<10)?("0"+indexDay):(indexDay);
 			htmlDays += "<div class=\"day\" id=\"day_"+day2digits+"\">"+day2digits+"</div>";
 		}
 		lineOfDays.html(htmlDays);
 		
 		// All groups and ressources, prepare content
-		for(var indexGroup=0;indexGroup<this.ressources.groups.length;indexGroup++){
-			var group = this.ressources.groups[indexGroup];
+		for(indexGroup=0;indexGroup<this.ressources.groups.length;indexGroup++){
+			group = this.ressources.groups[indexGroup];
 			horizontalCalendarContent.append("<div data-group=\"group_"+group.id+"\" class=\"group\">&nbsp;</div>");
 			
-			for(var indexRessource=0;indexRessource<group.ressources.length;indexRessource++){
-				var ressource = group.ressources[indexRessource];
+			for(indexRessource=0;indexRessource<group.ressources.length;indexRessource++){
+				ressource = group.ressources[indexRessource];
 				horizontalCalendarContent.append("<div class=\"lineForRessource\" data-ressource=\"ressource_"+ressource.id+"\" id=\"events_r_"+ressource.id+"\"></div>");
 			}
 		}
@@ -191,17 +220,19 @@ $.extend(Event.prototype, {
 	},
 
 	drawIn: function(containerId) {
-		var margin = 50 * (this.startDay - 1);
-		var width = 50 * (this.endDay - this.startDay) - 3;
+		var margin, width;
+		margin = 50 * (this.startDay - 1);
+		width = 50 * (this.endDay - this.startDay) - 3;
 		
 		$("#"+containerId).find("#events_r_"+this.ressourceId).append('<div id="'+this.eventId+'" class="event" style="left: '+margin+'px;width:'+width+'px;">'+this.label+'</div>');
 		this.jObject = $("#"+this.eventId);
 		$("#"+containerId).find("#"+this.eventId).draggable({ axis: "x", containment: "parent", opacity: 0.5, snap: true });
 		$("#"+containerId).find("#events_r_"+this.ressourceId).droppable({
 			drop: function(event, ui){
-				var containerLeft = $(this).offset().left;
-				var eventLeft = ui.offset.left;
-				var newDayStart = 1 + ((eventLeft - containerLeft) / 50); // px
+				var containerLeft, eventLeft, newDayStart;
+				containerLeft = $(this).offset().left;
+				eventLeft = ui.offset.left;
+				newDayStart = 1 + ((eventLeft - containerLeft) / 50); // px
 				$(".debug").html("newDayStart: "+  newDayStart );
 			}
 		});
